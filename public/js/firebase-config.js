725c2fc954487f2723e4670d655a804e61d9ed69
neilvocea <futbolnap01@gmail.com>
2026-09-16 19:55:23 -0500

    Add Firebase configuration export

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyB65iB8MoH9zOV_Vk6JkcwefIOwK8hbHLc",
+  authDomain: "vocea-61c58.firebaseapp.com",
+  projectId: "vocea-61c58",
+  storageBucket: "vocea-61c58.firebasestorage.app",
+  messagingSenderId: "1027332349096",
+  appId: "1:1027332349096:web:abee8cad0baa3aedc2769d",
+  measurementId: "G-JX854Z66EM",
+};
